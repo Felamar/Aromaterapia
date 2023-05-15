a40c3531cf0b15228b109f177edbca40ecd3652e
Felamar <114794450+Felamar@users.noreply.github.com>
2023-05-15 06:54:26 -0600

    Project Done

--- aromaterapia.py
+++ aromaterapia.py
@@ -1,32 +1,31 @@
 import os
 import re
 import pandas as pd
-import numpy as np
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from tkinter import filedialog
-from Lproducts import *
+from tkinter import messagebox
+
+class producto:
+    def __init__(self):
+
+        self.code           = int   ()
+        self.description    = str   ()
+        self.price          = float ()
+        self.benefits       = list  ()
+        self.duration       = int   ()
+        self.img            = str   ()
 
 class App(ttk.Frame):
 
-    # CONSTANTS
+    # GLOBAL VARIABLES
     db               = dict()
+    attributes_db    =dict()
     temp_product     = producto()
     temp_product.img = None
+
+    # CONSTANTS
     db_keys          = ["description", "price", "benefits", "duration", "img"]
-    df_products      = pd.read_csv("products.csv")
-
-    for index, row in df_products.iterrows():
-        product = producto()
-        product.code        = row["code"]
-        product.description = row["description"]
-        product.price       = row["price"]
-        product.benefits    = row["benefits"].split(",")
-        product.duration    = row["duration"]
-        product.img         = row["img"]
-        db[product.code]    = product
-
-
     CODE, DESCRIPTION, PRICE, BENEFITS, DURATION, IMG = 0, 1, 2, 3, 4, 5
     P_DES, P_EXA = 0, 1
     PARAMETERS = {
@@ -44,8 +43,24 @@ class App(ttk.Frame):
         BENEFITS    : [r"^[a-zA-Z0-9 ]+(,[a-zA-Z0-9 ]+)*$", 20], 
         DURATION    : [r"^\d{1,3}$",                         6] 
     }
-##########################################################################################
 
+    # DATABASE INITIALIZATION
+    if not os.path.isfile("products.csv"):
+        df = pd.DataFrame(columns=["code"] + db_keys)
+        df.to_csv('products.csv', index=False)
+    else:
+        df_products      = pd.read_csv("products.csv")
+        for index, row in df_products.iterrows():
+            product = producto()
+            product.code        = row["code"]
+            product.description = row["description"]
+            product.price       = row["price"]
+            product.benefits    = row["benefits"].split(",")
+            product.duration    = row["duration"]
+            product.img         = row["img"]
+            db[product.code]    = product
+
+    # ATTRIBUTES INITIALIZATION
     def __init__(self, master):
         
         super().__init__(master)
@@ -69,19 +84,20 @@ class App(ttk.Frame):
 
         # Register frame
         register_text = 'Complete los campos para registrar un producto'
-        register_lf = ttk.Labelframe(main_container, text = register_text, padding=(15,10,15,10))
+        register_lf = ttk.Labelframe(main_container, text = register_text, padding=(15,10,10,10))
         register_lf.configure(style="info.TLabelframe")
         self.frames["register"] = register_lf
         register_lf.grid(row = 0, column = 0, sticky = "nsew")
 
         # Modify frame
         modify_text = 'Complete los campos para modificar un producto'
-        modify_lf = ttk.Labelframe(main_container, text = modify_text, padding=(15,10,15,10))
+        modify_lf = ttk.Labelframe(main_container, text = modify_text, padding=(15,10,10,10))
         modify_lf.configure(style="info.TLabelframe")
         self.frames["modify"] = modify_lf
         modify_lf.grid(row = 0, column = 0, sticky = "nsew")
 
         # Show register frame
+        self.image_labels = {}
         self.show_lf("register")
         self.register_entries ={}
         self.register_fields()
@@ -89,9 +105,8 @@ class App(ttk.Frame):
         self.modify_fields()
         master.update()
         master.minsize(master.winfo_width(), master.winfo_height())
-        master.maxsize(master.winfo_width(), master.winfo_height())
-##########################################################################################
 
+    # METHODS
     def create_sidebar_btns(self):
 
         register_btn = ttk.Button(
@@ -111,7 +126,6 @@ class App(ttk.Frame):
         )
         self.sidebar_btns["modify"] = modify_btn
         modify_btn.pack(fill = X, pady = (10,0), padx = (5,0))
-##########################################################################################
 
     def register_fields(self):
 
@@ -129,6 +143,7 @@ class App(ttk.Frame):
             if P == App.IMG:
                 label.configure(text = "")
                 label.grid(row=0, column=P, sticky="w", padx=5)
+                self.image_labels["register"] = label
                 entry = ttk.Button(register_frame,
                     text=App.PARAMETERS[P][App.P_DES], 
                     command=lambda label = label: self.get_img(label)
@@ -161,11 +176,10 @@ class App(ttk.Frame):
 
             register_btn = ttk.Button(register_frame, 
                 text="Registrar", 
-                command=lambda: self.get_values(self.register_entries)
+                command=lambda: self.get_values(self.register_entries, "register")
             )
             register_btn.configure(style="success.TButton")
             register_btn.grid(row=2, column=0, padx=5, pady=(15,0), sticky="w", columnspan=2)
-##########################################################################################
 
     def modify_fields(self):
         modify_frame = ttk.Frame(self.frames["modify"])
@@ -182,11 +196,12 @@ class App(ttk.Frame):
             if P == App.IMG:
                 label.configure(text = "")
                 label.grid(row=2, column=P-1, sticky="w", padx=5)
+                self.image_labels["modify"] = label
                 entry = ttk.Button(modify_frame,
                     text=App.PARAMETERS[P][App.P_DES], 
                     command=lambda label = label: self.get_img(label)
                 )
-                entry.grid(row=3, column=P, padx=5)
+                entry.grid(row=3, column=P-1, padx=5)
                 entry.configure(style="info.TButton")
                 self.modify_entries[P] = entry
                 continue
@@ -219,18 +234,17 @@ class App(ttk.Frame):
 
             search_btn = ttk.Button(modify_frame,
                 text="Buscar",
-                command=lambda: self.get_values(self.modify_entries)
+                command=lambda: self.search_product(self.modify_entries)
             )
             search_btn.configure(style="info.TButton")
             search_btn.grid(row=1, column=0, padx=(73, 5), pady=(0,15), sticky="w")
 
             modify_btn = ttk.Button(modify_frame, 
                 text="Modificar", 
-                command=lambda: self.get_values(self.modify_entries)
+                command=lambda: self.get_values(self.modify_entries, "modify")
             )
             modify_btn.configure(style="success.TButton")
-            modify_btn.grid(row=4, column=0, padx=5, pady=(15,0), sticky="w", columnspan=2)
-##########################################################################################
+            modify_btn.grid(row=3, column=5, padx=5)
 
     def show_lf(self, lf_name):
 
@@ -241,7 +255,6 @@ class App(ttk.Frame):
                 self.sidebar_btns[btn].configure(bootstyle = PRIMARY)
             else:
                 self.sidebar_btns[btn].configure(bootstyle = OUTLINE)
-##########################################################################################
 
     def entry_focus(self, event, entry, default_text):
 
@@ -251,7 +264,6 @@ class App(ttk.Frame):
         elif entry.get() == "":
             entry.insert(0, default_text)
             entry.config(foreground="#ababab")
-##########################################################################################
 
     def get_img(self, label):
         home_dir = os.path.expanduser("~")
@@ -266,9 +278,8 @@ class App(ttk.Frame):
             App.temp_product.img = image_path
         else:
             App.temp_product.img = None
-##########################################################################################
 
-    def get_values(self, entries):
+    def get_values(self, entries, method):
         error_index = None
         error_count = 0
         for P in entries.keys():
@@ -308,13 +319,36 @@ class App(ttk.Frame):
         App.temp_product.benefits    =       entries[App.BENEFITS   ].get().split(",")
         App.temp_product.duration    = int  (entries[App.DURATION   ].get())
 
-        self.register_Product(App.temp_product, App.db)
-##########################################################################################
+        if method == "modify":
+            self.modify_Product(App.temp_product, App.db)
+        elif method == "register":
+            self.register_Product(App.temp_product, App.db)
 
     def modify_Product(self, product, db):
+        if not messagebox.askyesno("Alert", "¿Desea modificar el producto {}?".format(product.code)):
+            return
+        db[product.code] = product
+        App.attributes_db[product.code] = [
+            product.description,
+            product.price,
+            product.benefits,
+            product.duration,
+            product.img
+        ]
+        #write db to csv with pandas, each column is a parameter of the product
+        df = pd.DataFrame.from_dict(App.attributes_db, orient='index', columns=App.db_keys)
+        df['benefits'] = df['benefits'].apply(lambda x: ','.join(x))
+        df.to_csv("products.csv", index_label="code")
 
-        pass
-##########################################################################################
+        messagebox.showinfo("Alert", "Producto {} modificado con éxito".format(product.code))
+
+        for P in App.PARAMETERS:
+            if P == App.IMG:
+                self.image_labels["modify"].configure(text = "")
+                continue
+            self.modify_entries[P].delete(0, "end")
+            self.modify_entries[P].insert(0, App.PARAMETERS[P][App.P_EXA])
+            self.modify_entries[P].config(foreground="#ababab")
 
     def register_Product(self, product, db):
 
@@ -324,7 +358,8 @@ class App(ttk.Frame):
                 self.modify_Product(product, db)
             return
         else:
-            db[product.code] = [
+            db[product.code] = product
+            App.attributes_db[product.code] = [
                 product.description,
                 product.price,
                 product.benefits,
@@ -332,16 +367,48 @@ class App(ttk.Frame):
                 product.img
             ]
             #write db to csv with pandas, each column is a parameter of the product
-            df = pd.DataFrame.from_dict(db, orient='index')
-            df.to_csv("products.csv", header=App.db_keys, index_label="code")
+            df = pd.DataFrame.from_dict(App.attributes_db, orient='index', columns=App.db_keys)
+            df['benefits'] = df['benefits'].apply(lambda x: ','.join(x))
+            df.to_csv("products.csv", index_label="code")
 
+        messagebox.showinfo("Alert", "Producto {} registrado con éxito".format(product.code))
 
+        for P in App.PARAMETERS:
+            if P == App.IMG:
+                self.image_labels["register"].configure(text = "")
+                continue
+            self.register_entries[P].delete(0, "end")
+            self.register_entries[P].insert(0, App.PARAMETERS[P][App.P_EXA])
+            self.register_entries[P].config(foreground="#ababab")
 
+    def search_product(self, entries):
+        code = int(entries[App.CODE].get())
+        if code in App.db.keys():
+            
+            App.temp_product.code        = code
+            App.temp_product.description = App.db[code].description
+            App.temp_product.price       = App.db[code].price
+            App.temp_product.benefits    = App.db[code].benefits
+            App.temp_product.duration    = App.db[code].duration
+            App.temp_product.img         = App.db[code].img
 
-        messagebox.showinfo("Alert", "Producto registrado con éxito")
-##########################################################################################
+            entries[App.DESCRIPTION].delete(0, "end")
+            entries[App.DESCRIPTION].insert(0, App.temp_product.description)
+            entries[App.DESCRIPTION].config(foreground="#232323")
+            entries[App.PRICE      ].delete(0, "end")
+            entries[App.PRICE      ].insert(0, App.temp_product.price)
+            entries[App.PRICE      ].config(foreground="#232323")
+            entries[App.BENEFITS   ].delete(0, "end")
+            entries[App.BENEFITS   ].insert(0, ','.join(App.temp_product.benefits))
+            entries[App.BENEFITS   ].config(foreground="#232323")
+            entries[App.DURATION   ].delete(0, "end")
+            entries[App.DURATION   ].insert(0, App.temp_product.duration)
+            entries[App.DURATION   ].config(foreground="#232323")
+            self.image_labels["modify"].configure(text = App.temp_product.img.split("/")[-1])
 
-
+        else:
+            messagebox.showinfo("Alert", "Producto {} no encontrado".format(code))
+        pass
 
 if __name__ == '__main__':
     app = ttk.Window(themename = "minty")
